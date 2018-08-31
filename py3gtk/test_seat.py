827607357d44ed98371861fa596159e879cef39a
ice0 <konoplin@gmail.com>
2018-08-31 20:42:35 +0700

    Changes

--- py3gtk/test_seat.py
+++ py3gtk/test_seat.py
@@ -1,23 +1,36 @@
 import gi
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+#gi.require_version('Gtk', '3.0')
+gi.require_version('Gdk', '3.0')
+#from gi.repository import Gtk
 from gi.repository import Gdk
 
 #slaves = Gdk.Seat().get_slaves()
+print("Get default display...")
 display = Gdk.Display().get_default()
 if (display == None):
     print("Display not found!")
     exit
 
+print("Get default seat...")
 seat = display.get_default_seat()
 if (seat == None):
     print("Default seat not found!")
     exit
 
+print("Get slaves...")
 #slaves = seat.get_slaves(Gdk.SeatCapabilities.ALL)
-slaves = seat.get_slaves(Gdk.SeatCapabilities.ALL_POINTING)
+slaves = seat.get_slaves(Gdk.SeatCapabilities.NONE)
+if (slaves == None):
+    print("Slaves not found!")
+    exit
+    
+print("Found slaves (" + str(len(slaves)) + ")")
 
-for slave in slaves:
-    print(slave.get_name())
+#slaves = seat.get_slaves(Gdk.SeatCapabilities.ALL_POINTING)
 
-Gtk.main()
+#for slave in slaves:
+#    print(slave.get_name())
+
+print("================================")
+
+#Gtk.main()
