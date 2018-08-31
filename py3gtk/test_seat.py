15c87233084e59f53b89eb57b33204d49f2bb30b
ice0 <konoplin@gmail.com>
2018-08-31 21:20:07 +0700

    update

--- py3gtk/test_seat.py
+++ py3gtk/test_seat.py
@@ -52,6 +52,7 @@ print("================================")
 #Gtk.main()
 
 # MacOS output
+# - - - - - - - - - - - - - - - 
 #Number of displays(0)
 #Get default display...
 #./test_seat.py:21: DeprecationWarning: Gdk.Display.get_device_manager is deprecated
@@ -65,3 +66,34 @@ print("================================")
 #Get slaves...
 #Found slaves (0)
 #Core Pointer
+
+# Linux output
+# - - - - - - - - - - - - - -
+#xinput list
+#⎡ Virtual core pointer                    	id=2	[master pointer  (3)]
+#⎜   ↳ Virtual core XTEST pointer              	id=4	[slave  pointer  (2)]
+#⎜   ↳ VMware VMware Virtual USB Mouse         	id=7	[slave  pointer  (2)]
+#⎜   ↳ VirtualPS/2 VMware VMMouse              	id=9	[slave  pointer  (2)]
+#⎜   ↳ VirtualPS/2 VMware VMMouse              	id=10	[slave  pointer  (2)]
+#⎣ Virtual core keyboard                   	id=3	[master keyboard (2)]
+#    ↳ Virtual core XTEST keyboard             	id=5	[slave  keyboard (3)]
+#    ↳ Power Button                            	id=6	[slave  keyboard (3)]
+#    ↳ AT Translated Set 2 keyboard            	id=8	[slave  keyboard (3)]
+#$ python ./test_seat.py 
+#Number of displays(0)
+#Get default display...
+#./test_seat.py:21: DeprecationWarning: Gdk.Display.get_device_manager is deprecated
+#  devices = display.get_device_manager().list_devices(Gdk.DeviceType.MASTER)
+#./test_seat.py:21: DeprecationWarning: Gdk.DeviceManager.list_devices is deprecated
+#  devices = display.get_device_manager().list_devices(Gdk.DeviceType.MASTER)
+#Number of devices(2)
+#Virtual core keyboard
+#Virtual core pointer
+#Get default seat...
+#Get slaves...
+#Found slaves (4)
+#VMware VMware Virtual USB Mouse
+#VirtualPS/2 VMware VMMouse
+#VirtualPS/2 VMware VMMouse
+#Virtual core XTEST pointer
+#Virtual core pointer
